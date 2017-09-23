851ccffac8d4839a391c3881d78a8f1125383d31
Cassius Pacheco <cassius.pacheco@theiconic.com.au>
2017-09-24 00:16:56 +1000

    Run MarketJob once a day

--- Sources/App/Droplet+Setup.swift
+++ Sources/App/Droplet+Setup.swift
@@ -1,8 +1,22 @@
 @_exported import Vapor
+import Jobs
 
 extension Droplet {
+    
     public func setup() throws {
+        
         try setupRoutes()
         // Do any additional droplet setup
+        
+        setupJobs()
+    }
+    
+    private func setupJobs() {
+        
+        Jobs.add(interval: .days(1)) {
+            
+            let job = MarketJob()
+            job.fetchAndSaveMarkets()
+        }
     }
 }
