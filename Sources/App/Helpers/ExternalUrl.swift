7ff160d8d21f410da567198478ab3825220932f5
Cassius Pacheco <cassius.pacheco@theiconic.com.au>
2017-09-24 00:11:43 +1000

    Add ExternalUrl file

---
+++ Sources/App/Helpers/ExternalUrl.swift
@@ -0,0 +1,31 @@
+//
+//  ExternalUrl.swift
+//  sweettrex
+//
+//  Created by Cassius Pacheco on 23/9/17.
+//
+//
+
+import Foundation
+
+enum ExternalUrl {
+    
+    case market
+    case ticker(Market)
+    
+    private static let bittrexBaseUrl = "https://bittrex.com/api/v1.1/public"
+    
+    var url: URL {
+        
+        switch self {
+            
+        case .market:
+            
+            return URL(string: ExternalUrl.bittrexBaseUrl + "/getmarkets")!
+            
+        case .ticker(let market):
+            
+            return URL(string: ExternalUrl.bittrexBaseUrl + "/public/getticker?market=" + market.name)!
+        }
+    }
+}
