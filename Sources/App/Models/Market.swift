5247b5ca280b9a653d5a7869aeaf14131cffe5a3
Cassius Pacheco <cassius.pacheco@theiconic.com.au>
2017-09-23 20:44:12 +1000

    Update market to support description

--- Sources/App/Models/Market.swift
+++ Sources/App/Models/Market.swift
@@ -12,17 +12,22 @@ import HTTP
 struct Market: JSONInitializable {
     
     let name: String
+    let description: String
     let isActive: Bool
     
-    init(name: String, isActive: Bool = true) {
+    init(name: String, description: String, isActive: Bool = true) {
         
         self.name = name
+        self.description = description
         self.isActive = isActive
     }
     
     init(json: JSON) throws {
         
+        guard let base: String = try json.get("BaseCurrencyLong"), let currency: String = try json.get("MarketCurrencyLong") else { throw Abort.badRequest }
+        
         self.name = try json.get("MarketName")
         self.isActive = try json.get("IsActive")
+        self.description = base + "-" + currency
     }
 }
